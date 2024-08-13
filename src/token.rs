9f678310be1f8656ef364ebe896c7089adf70c31
Davide Nale <davide.nale.99@gmail.com>
2024-08-13 15:28:53 +0200

    Moved to enum for Token, implemented Display trait

--- src/token.rs
+++ src/token.rs
@@ -1,13 +1,23 @@
+use std::fmt;
+
+use phf::phf_map;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum TokenType {
-    Illegal,
-    Eof,
+pub enum Token {
     // Identifiers + literals
     Ident(String),
-    Int(String),
+    Int(i64),
     // Operators
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Lt,
+    Gt,
+    Eq,
+    NotEq,
     // Delimiters
     Comma,
     Semicolon,
@@ -18,19 +28,45 @@ pub enum TokenType {
     // Keywords
     Function,
     Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+    // Special tokens
+    Illegal(char),
+    Eof,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Token {
-    pub token_type: TokenType,
-    pub literal: String,
-}
-
-impl Token {
-    pub fn new(token_type: TokenType, literal: &str) -> Self {
-        Token {
-            token_type,
-            literal: literal.to_string(),
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::Ident(s) | Token::Int(s) => write!(f, "{}", s),
+            Token::Assign => write!(f, "="),
+            Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Bang => write!(f, "!"),
+            Token::Asterisk => write!(f, "*"),
+            Token::Slash => write!(f, "/"),
+            Token::Lt => write!(f, "<"),
+            Token::Gt => write!(f, ">"),
+            Token::Eq => write!(f, "=="),
+            Token::NotEq => write!(f, "!="),
+            Token::Comma => write!(f, ","),
+            Token::Semicolon => write!(f, ";"),
+            Token::LParen => write!(f, "("),
+            Token::RParen => write!(f, ")"),
+            Token::LBrace => write!(f, "{{"),
+            Token::RBrace => write!(f, "}}"),
+            Token::Function => write!(f, "fn"),
+            Token::Let => write!(f, "let"),
+            Token::True => write!(f, "true"),
+            Token::False => write!(f, "false"),
+            Token::If => write!(f, "if"),
+            Token::Else => write!(f, "else"),
+            Token::Return => write!(f, "return"),
+            Token::Illegal(c) => write!(f, "Illegal({})", c),
+            Token::Eof => write!(f, "EOF"),
         }
     }
 }
