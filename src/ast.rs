e63f45f6780c3fc5025bbfea0930355f98d4ffb8
Davide Nale <davide.nale.99@gmail.com>
2024-09-09 10:41:40 +0200

    Expanded ast with expressions

--- src/ast.rs
+++ src/ast.rs
@@ -2,15 +2,20 @@ use std::fmt;
 
 use crate::token;
 
+#[derive(Default)]
 pub struct Ast {
     pub statements: Vec<Statement>,
 }
 
 impl Ast {
     pub fn new() -> Self {
-        Ast {
-            statements: Vec::new(),
-        }
+        Ast::default()
+    }
+}
+
+impl From<Vec<Statement>> for Ast {
+    fn from(statements: Vec<Statement>) -> Self {
+        Self { statements }
     }
 }
 
@@ -60,7 +65,9 @@ pub enum Statement {
 impl fmt::Display for Statement {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Statement::Let { token, expr } => todo!(),
+            Statement::Let { token, expr } => {
+                write!(f, "let {} = {};", token.to_string(), expr.to_string())
+            }
             Statement::Return { token, expr } => todo!(),
             Statement::Expression { token, expr } => todo!(),
             Statement::Block { body } => todo!(),
@@ -69,11 +76,53 @@ impl fmt::Display for Statement {
 }
 
 pub enum Expression {
-    // Identifier(Ident),
-    // Boolean(Bool),
-    // Prefix(),
-    // Infix(),
-    // If(),
-    // Function(),
-    // Call(),
+    Identifier(String),
+    Boolean(bool),
+    Int(String),
+    PrefixExpr {
+        op: token::Token,
+        right: Box<Expression>,
+    },
+    InfixExpr {
+        op: token::Token,
+        left: Box<Expression>,
+        right: Box<Expression>,
+    },
+    If {
+        condition: Box<Expression>,
+        consequence: Box<Statement>,
+        alternative: Option<Box<Statement>>,
+    },
+    Function {
+        parameters: Vec<Expression>,
+        body: Box<Statement>,
+    },
+    Call {
+        function: Box<Expression>,
+        arguments: Box<Expression>,
+    },
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expression::Int(s) => write!(f, "{}", s),
+            _ => todo!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ast_display() {
+        let ast = Ast::from(vec![Statement::Let {
+            expr: Expression::Int("5".to_string()),
+            token: token::Token::Ident("x".to_string()),
+        }]);
+
+        assert_eq!(ast.to_string(), "let x = 5;");
+    }
 }
